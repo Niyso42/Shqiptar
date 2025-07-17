675a83b545244369f8f30e890af981328351e417
Yusuf <crieeslam@gmail.com>
2025-07-18 01:33:27 +0200

    Makefile

--- exec/exec.c
+++ exec/exec.c
@@ -57,13 +57,49 @@ static void	handle_empty_argv(char **argv, t_data *data)
 		cleanup_and_exit(argv, data, 0);
 }
 
+static void	handle_execve_error(char **argv)
+{
+	if (errno == EACCES)
+	{
+		ft_putstr_fd(argv[0], 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+	}
+	else if (errno == ENOEXEC)
+	{
+		ft_putstr_fd(argv[0], 2);
+		ft_putstr_fd(": cannot execute binary file: Exec format error\n", 2);
+	}
+	else if (errno == ENOENT)
+	{
+		ft_putstr_fd(argv[0], 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+	}
+	else if (errno == EISDIR)
+	{
+		ft_putstr_fd(argv[0], 2);
+		ft_putstr_fd(": Is a directory\n", 2);
+	}
+	else
+		perror("execve failed");
+}
+
 void	execute_cmd(char *path, char **argv, t_data *data)
 {
 	if (execve(path, argv, data->env->env) == -1)
 	{
-		perror("execve failed");
-		exit(126);
+		handle_execve_error(argv);
+		free(path);
+		cleanup_and_exit(argv, data, 126);
 	}
+}
+
+static int	is_directory(char *path)
+{
+	struct stat	stat_buf;
+
+	if (stat(path, &stat_buf) == 0 && S_ISDIR(stat_buf.st_mode))
+		return (1);
+	return (0);
 }
 
 char	*prepare_path(char *cmd, t_data *data)
@@ -74,8 +110,15 @@ char	*prepare_path(char *cmd, t_data *data)
 	if (!cmd || !cmd[0])
 		return (NULL);
 	argv = ft_split(cmd, ' ');
-	if (access(argv[0], X_OK) == 0)
+	if (access(argv[0], F_OK) == 0)
+	{
+		if (is_directory(argv[0]))
+		{
+			free_tab(argv);
+			return (NULL);
+		}
 		path = ft_strdup(argv[0]);
+	}
 	else if (data->env)
 		path = get_path(argv[0], data, argv);
 	else
@@ -233,7 +276,6 @@ static void	handle_command_execution(char **argv, t_data *data)
 	if (!path)
 		handle_command_not_found(argv, data);
 	execute_cmd(path, argv, data);
-	free(path);
 	cleanup_and_exit(argv, data, 126);
 }
 
--- includes/minishell.h
+++ includes/minishell.h
@@ -23,6 +23,7 @@
 # include <stdlib.h>
 # include <string.h>
 # include <sys/types.h>
+# include <sys/stat.h>
 # include <termios.h>
 # include <sys/wait.h>
 # include <unistd.h>
